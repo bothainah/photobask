310e3bd74c3b5e4453916d64cec2f1da7505929b
bothainah <38307213+bothainah@users.noreply.github.com>
2018-04-12 07:40:39 +0300

    Delete script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.project-thumbnail').hover(function(){
-        $(this).find('.project-thumbnail-overlay').slideToggle();
-        $(this).find('.project-category').slideToggle();
-    });
-});
